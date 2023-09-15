9b8f952809089ce1a6114a768bc577a15deb8756
go-dev <awwalcodestar@gmail.com>
2023-09-15 23:19:45 +0100

    .

--- pkg/config/personConfig.go
+++ pkg/config/personConfig.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	dsn := "host=dpg-ck2bmcfhdsdc73a05o8g-a port=5432 user=record_h59m_user dbname=RECORD password=FZ9EuZHcJH1iX8qwkOPZSPP0JCQXG6lO sslmode=disable"
+	dsn := "host=dpg-ck2bmcfhdsdc73a05o8g-a port=5432 user=record_h59m_user dbname=record_h59m password=FZ9EuZHcJH1iX8qwkOPZSPP0JCQXG6lO sslmode=disable"
 	d, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
